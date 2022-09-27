c32d7df75fe65fec58f9098ee5ec58f09ee5f2ba
Saimon Ribeiro <saimonribeiros@hotmail.com>
2022-09-26 22:59:55 -0300

    add config

--- src/pages/Configuration/Configuration.jsx
+++ src/pages/Configuration/Configuration.jsx
@@ -1,10 +1,63 @@
+import React, {useEffect, useState} from "react";
+import {HttpGetAxios, HttpPostAxios} from "../../http/HttpBasicAxios";
+import {ErrorToast, SuccessToast} from "../../components/Toasty/Toasty";
 
 
 const Configuration = () => {
-    return(
-        <div>
-            CONFIGURACAO
-        </div>
+    const [allUsers, setAllUsers] = useState([])
+
+    useEffect(() => {
+        getAllUsers()
+    }, [])
+
+    function getAllUsers(){
+        HttpGetAxios("/user")
+            .then(resp => {
+                console.log("req")
+                setAllUsers(resp.data)
+            })
+    }
+
+    function promoteToAdmin(id, username) {
+        HttpPostAxios("/user/" + id + "/promoted")
+            .then(() => {
+                SuccessToast(username + " is admin now!")
+                getAllUsers()
+            })
+            .catch(() => {
+                ErrorToast("Have a problem with API")
+            })
+        console.log(allUsers)
+    }
+
+    return (
+        <table className="table table-hover">
+            <thead>
+            <tr>
+                <th scope="col"></th>
+                <th scope="col">id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Username</th>
+                <th scope="col">Role</th>
+            </tr>
+            </thead>
+            <tbody>
+            {allUsers.map((user, index) => (
+                <tr>
+                    <th scope="row">{index + 1}</th>
+                    <td>{user._id}</td>
+                    <td>{user.name + " " + user.lastName}</td>
+                    <td>{user.username}</td>
+                    <td>{user.roles.includes("ADMIN") === true ?
+                        <button type="button" className="btn btn-success">Admin</button>
+                        :
+                        <button type="button" className="btn btn-secondary"
+                                onClick={() => promoteToAdmin(user._id, user.username)}>Admin</button>
+                    }</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
     )
 }
 
